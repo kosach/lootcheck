2f5f38cd3e39a39a454feeacdf3a6e238f93e522
Andrii Oleksiyovych Kosach <andrii.o.kosach@globallogic.com>
2017-10-19 16:36:56 +0300

    Add wallet component; Save balance too cookie

---
+++ .history/src/actions/constants_20171018175423.js
@@ -0,0 +1,1 @@
+const SET_BALANCE = 'SET_BALANCE';
---
+++ .history/src/reducers/balance_20171019162635.js
@@ -0,0 +1,19 @@
+import * as constants from '../actions/constants';
+import { read_cookie, bake_cookie } from 'sfcookies';
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE';
+
+const balance = ( state = 0, action) => {
+  switch (action.type) {
+    case constants.SET_BALANCE:
+      return action.balance;
+    case constants.DEPOSIT:
+      return state + action.deposit;  
+    case constants.WITHDRAW:
+      return state - action.withdrawal;  
+    default:
+      return read_cookie(BALANCE_COOKIE);
+  }
+}
+
+export default balance;
